68a5ee08c0228a10d8748439fde05b57bd2dc183
Danny <dzmen@hotmail.com>
2016-04-20 12:38:59 +0200

    Aanpassing KBS file

--- DE2_115_SD_Card_Audio_Player/Software/KBS/definitions.h
+++ DE2_115_SD_Card_Audio_Player/Software/KBS/definitions.h
@@ -1,10 +1,6 @@
 #ifndef __DEFINITIONS_H__
 #define __DEFINITIONS_H__
 
-//===== UCOS include
-
-#include "includes.h"
-
 //===== Terasic includes
 
 #include ".\terasic_lib\terasic_includes.h"
@@ -41,5 +37,27 @@ typedef unsigned long long alt_u64;
 // return number of wave file
 #define MAX_FILE_NUM    128
 #define FILENAME_LEN    32
+
+typedef struct{
+    int nFileNum;
+    char szFilename[MAX_FILE_NUM][FILENAME_LEN];
+}WAVE_PLAY_LIST;
+
+static WAVE_PLAY_LIST gWavePlayList;
+#define WAVE_BUF_SIZE  512  // do not chagne this constant (FIFO: 4*128 byte)
+typedef struct{
+    FAT_FILE_HANDLE hFile;
+    alt_u8          szBuf[WAVE_BUF_SIZE];  // one sector size of sd-card
+    alt_u32         uWavePlayIndex;
+    alt_u32         uWaveReadPos;
+    alt_u32         uWavePlayPos;
+    alt_u32         uWaveMaxPlayPos;
+    char szFilename[FILENAME_LEN];
+    alt_u8          nVolume;
+    bool            bRepeatMode;
+}PLAYWAVE_CONTEXT;
+
+static PLAYWAVE_CONTEXT gWavePlay;// = {{0},{0}, HW_DEFAULT_VOL, {0}, 0, 0, 0};
+static FAT_HANDLE hFat;
 
 #endif
--- DE2_115_SD_Card_Audio_Player/Software/KBS/functions.h
+++ DE2_115_SD_Card_Audio_Player/Software/KBS/functions.h
@@ -10,29 +10,6 @@
 
 #include "definitions.h"
 
-typedef struct{
-    int nFileNum;
-    char szFilename[MAX_FILE_NUM][FILENAME_LEN];
-}WAVE_PLAY_LIST;
-
-
-static WAVE_PLAY_LIST gWavePlayList;
-#define WAVE_BUF_SIZE  512  // do not chagne this constant (FIFO: 4*128 byte)
-typedef struct{
-    FAT_FILE_HANDLE hFile;
-    alt_u8          szBuf[WAVE_BUF_SIZE];  // one sector size of sd-card
-    alt_u32         uWavePlayIndex;
-    alt_u32         uWaveReadPos;
-    alt_u32         uWavePlayPos;
-    alt_u32         uWaveMaxPlayPos;
-    char szFilename[FILENAME_LEN];
-    alt_u8          nVolume;
-    bool            bRepeatMode;
-}PLAYWAVE_CONTEXT;
-
-static PLAYWAVE_CONTEXT gWavePlay;// = {{0},{0}, HW_DEFAULT_VOL, {0}, 0, 0, 0};
-static FAT_HANDLE hFat;
-
 //===== function prototype =====
 void update_status(void);
 void wait_sdcard_insert(void);
